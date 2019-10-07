16563d46b8a568be44561d56b488319ed3bffb14
MarilynDafa <marilyndafa@163.com>
2019-10-07 16:28:20 +0800

    cleanup

--- samples/sample/main.c
+++ samples/sample/main.c
@@ -75,11 +75,63 @@ sapp_desc sokol_main(int argc, char* argv[]) {
 //framebuffer
 //srgb
 BLGuid s1;
+
+const char* code = "SSize=110\n\
+SSizeV=10\n\
+ESize=40\n\
+ESizeV=0\n\
+SSpin=720\n\
+SSpinV=0\n\
+ESpin=0\n\
+ESpinV=0\n\
+SR=255\n\
+SG=51\n\
+SB=120\n\
+SA=153\n\
+SRV=25\n\
+SGV=0\n\
+SBV=0\n\
+SAV=50\n\
+ER=255\n\
+EG=0\n\
+EB=0\
+EA=0\n\
+ERV=0\n\
+EGV=0\n\
+EBV=0\n\
+EAV=0\n\
+Amount=289\n\
+Duration=-1.0\n\
+Life=1.1\n\
+LifeV=0.4\n\
+Velocity=262\n\
+Angle=146\n\
+AngleV=50\n\
+Mode=Gravity\n\
+GVelocity=244\n\
+GVelocityV=5\n\
+GEmitterW=15\n\
+GEmitterH=-20\n\
+GX=-1000\n\
+GY=740\n\
+GRad=0\n\
+GRadV=0\n\
+GTan=0\n\
+GTanV=0\n\
+RMaxRadius=0\n\
+RMaxRadiusV=0\n\
+RMinRadius=0\n\
+RRot=0\n\
+RRotTime=0";
 const void BeginSystem(void)
 {
 	float x[2] = { 2.0, 4.0 };
 	float y[2] = { 2.0, 4.0 };
-	const char* tag = "a1,a2,a3,a4,a5,a6,a7,a8,a9";
+	blOpenPlugin("TMX");
+	BLBool (*_open)(IN BLAnsi*,IN BLAnsi*) = (BLBool(*)(IN BLAnsi*, IN BLAnsi*))blPluginProcAddress("TMX", "blTMXFileEXT");
+	_open("content/isometric_grass_and_water.tmx",NULL);
+
+
 	//s1 = blGenSprite("sss.bmg", NULL, tag, 640, 480, 12, 6, 0);
 	//s1 = blGenSprite("cursor.bmg", NULL, "cursor", 60, 60, 12, 1, 0);
 	//s1 = blGenSprite("uv.bmg", NULL, NULL, 200, 200, 12, 1, 0);
@@ -89,11 +141,6 @@ const void BeginSystem(void)
 	//blSpriteMove(s1, 400, 400);
 	//blSpriteGlow(s1, 0xFFdd8805, 3, 0);
 	//blSpriteFlip(s1, 1, 1, 1);
-	//blOpenPlugin("blTMX");
-
-	//BLBool (*_open)(IN BLAnsi*,IN BLAnsi*) = (BLBool(*)(IN BLAnsi*, IN BLAnsi*))blGetPluginProcAddress("blTMX", "blTMXFileEXT");
-	//blWorkingDir(FALSE);
-	//_open("m2.tmx",NULL);
 	//blSpriteScissor(s1, 0, 0, 390, 390);
 	//blSpriteAlpha(s1, 0.1, 1);
 	// = blGenSprite("s1", "", "s1", 12, 12, 23, 1);
